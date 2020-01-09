ba4814677c04fa4d96e1d02fde0127f211ef44f4
jiawu915 <59641230+jiawu915@users.noreply.github.com>
2020-01-09 14:26:41 +0800

    Create MyContacts.java

---
+++ app/src/main/java/com/example/zbrj/module/MyContacts.java
@@ -0,0 +1,32 @@
+package com.example.zbrj.module;
+
+public class MyContacts {
+
+    public String name;
+    public String phone;
+    public String note;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
